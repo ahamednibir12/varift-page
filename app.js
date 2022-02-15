ba7dc654576e4b1856e8d4dbaf8af35659d03742
ahamednibir12 <ahamednibir639@gmail.com>
2022-02-15 20:24:49 +0600

    second commit

--- app.js
+++ app.js
@@ -28,3 +28,14 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
   }
 
 })
+
+function verifyPin() {
+  const pin = document.getElementById('display-pin').value;
+  const typeNumber = document.getElementById('typed-numbers').value;
+  if (pin == typeNumber) {
+    console.log('oppps');
+  }
+  else {
+    console.log('wong number');
+  }
+}
